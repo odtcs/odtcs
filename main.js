ba9b9e05f4971b866ef2cd1aadb03f2d67f3de40
Jasper Boot <jasper@reboot4.nl>
2016-03-13 15:36:24 +0100

    Basic working version with programmes

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
 $(function() {
+	$('#choice-afternoon').click(function() {
+		start("afternoon");
+	});
+	$('#choice-morning').click(function() {
+		start("morning");
+	});
+});
+
+function start(type) {
+	$('#choice').remove();
+	$('#maximage').show()
+	$('.programme').not('.' + type).remove();
 	$('#maximage').maximage({
 		cycleOptions: {
 				fx: 'fade',
-				// Speed has to match the speed for CSS transitions
-				speed: 1500, 
-				timeout: 3000,
-				pause: 1
-			},
-        onFirstImageLoaded: function(){
-            jQuery('#cycle-loader').hide();
-            jQuery('#maximage').fadeIn('fast');
-        }
+				speed: 1000, 
+				timeout: 8000
+			}
 	});
-});
+}
