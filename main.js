6d3b392c6d30a981769b053ee7cab6c3e68fdce6
Jasper Boot <jasper@reboot4.nl>
2016-03-13 16:42:17 +0100

    Adds a video and fixes some bugs

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 $(function() {
+	jQuery('video, object').maximage('maxcover');
 	$('#choice-afternoon').click(function() {
 		start("afternoon");
 	});
@@ -15,7 +16,19 @@ function start(type) {
 		cycleOptions: {
 				fx: 'fade',
 				speed: 1000, 
-				timeout: 8000
-			}
+				timeout: 5000,
+				before: function(last,current){
+					//if(!$.browser.msie){
+						// Start HTML5 video when you arrive
+						if($(current).find('video').length > 0) $(current).find('video')[0].play();
+					//}
+				},
+				after: function(last,current){
+					//if(!$.browser.msie){
+						// Pauses HTML5 video when you leave it
+						if($(last).find('video').length > 0) $(last).find('video')[0].pause();
+					//}
+				}
+		}
 	});
 }
